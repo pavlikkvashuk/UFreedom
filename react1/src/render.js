12753d8bdf3feab0cd6f7dbf3225e7cf564feef7
Павло Квашук <33426981+pavlikkvashuk@users.noreply.github.com>
2022-11-20 11:07:24 +0200

    nrw file

---
+++ react1/src/render.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+
+import {addPost, updateNewPostText} from './redux/state';
+
+export let renderEntireTree = (state) => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App
+        posts={state.profilePage.posts}
+        dialogs={state.dialogsPage.dialogs}
+        messages={state.dialogsPage.messages}
+        addPost={addPost}
+        updateNewPostText = {updateNewPostText}
+      />
+    </React.StrictMode>
+  );
+}
+
+
+
+
+
+
