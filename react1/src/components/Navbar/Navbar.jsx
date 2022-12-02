7eba60f42134362c3584c0d7c1c5b6aa6b155642
pavlo <33426981+pavlikkvashuk@users.noreply.github.com>
2022-12-02 18:24:49 +0200

    refactoring

--- react1/src/components/Navbar/Navbar.jsx
+++ react1/src/components/Navbar/Navbar.jsx
@@ -9,6 +9,7 @@ const Navbar = () => {
     <nav className={s.nav}>
       <div className={s.item}><NavLink to="/profile" className={navData => navData.isActive ? s.active : s.item} >Profile</NavLink></div>
       <div className={s.item}><NavLink to="/dialogs" className={navData => navData.isActive ? s.active : s.item}>Messages</NavLink></div>
+      <div className={s.item}><NavLink to="/users" className={navData => navData.isActive ? s.active : s.item}>Users</NavLink></div>
       <div className={s.item}><NavLink to="/groups" className={navData => navData.isActive ? s.active : s.item}>Groups</NavLink></div>
       <div className={s.item}><NavLink to='/news' className={navData => navData.isActive ? s.active : s.item}>News</NavLink></div>
       <div className={s.item}><NavLink to='/settings' className={navData => navData.isActive ? s.active : s.item}>Settings</NavLink></div>
